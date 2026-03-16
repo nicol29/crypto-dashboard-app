b6a39c37a4c9983ccb1d805febf3314b8720e5d9
Nicolas Condrea <nicolascondrea@gmail.com>
2026-03-16 16:42:06 +0000

    dbup configuration

---
+++ backend/CryptoDashboard/CryptoDashboard.DbUp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddUserSecrets<Program>()
+    .Build();
+
+var connectionString = config["DBConnectionString"];
+
+var upgrader =
+    DeployChanges.To
+        .PostgresqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif                
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+
+return 0;

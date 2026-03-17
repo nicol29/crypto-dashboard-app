e3f4f5c07e7a31c0568e5ae045f76bb80d83aa38
Nicolas Condrea <nicolascondrea@gmail.com>
2026-03-17 14:34:09 +0000

    cleanup background service

--- backend/CryptoDashboard/CryptoDashboard.Api/Services/BinanceWorkerService.cs
+++ backend/CryptoDashboard/CryptoDashboard.Api/Services/BinanceWorkerService.cs
@@ -1,30 +1,42 @@
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using CryptoDashboard.Api.Models;
+using Microsoft.Extensions.Options;
 
 namespace CryptoDashboard.Api.Services;
 
-public class BinanceWorkerService(ILogger<BinanceWorkerService> logger) : BackgroundService
+public class BinanceWorkerService : BackgroundService
 {
-    private readonly ILogger<BinanceWorkerService> _logger = logger;
+    private readonly ILogger<BinanceWorkerService> _logger;
+    private readonly BinanceOptions _binanceOptions;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+    public BinanceWorkerService(ILogger<BinanceWorkerService> logger, IOptions<BinanceOptions> binanceOptions)
+    {
+        _logger = logger;
+        _binanceOptions = binanceOptions.Value;
+        _jsonSerializerOptions = new JsonSerializerOptions {
+            NumberHandling = JsonNumberHandling.AllowReadingFromString
+        };
+    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("Worker started at {time}", DateTimeOffset.Now);
         
         var client = new ClientWebSocket();
-        await client.ConnectAsync(new Uri("wss://stream.binance.com:9443/ws/btceur@ticker"), stoppingToken);
+        await client.ConnectAsync(new Uri(_binanceOptions.WebSocketUrl), stoppingToken);
         
         while (!stoppingToken.IsCancellationRequested && client.State == WebSocketState.Open)
         {
-            // _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
             var buffer = new byte[1024];
             var result = await client.ReceiveAsync(buffer, stoppingToken);
 
             var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-            var messageObj = JsonSerializer.Deserialize<BinanceTickerMessage>(message);
+            var tickerObj = JsonSerializer.Deserialize<BinanceTickerMessage>(message, _jsonSerializerOptions);
+            
             _logger.LogInformation("Message: {message}", message);
         }
     }

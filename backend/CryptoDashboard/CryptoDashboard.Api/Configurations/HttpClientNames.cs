0f49ad520cd4818f831a9b04e3f2e34d3d264eb0
Nicolas Condrea <nicolascondrea@gmail.com>
2026-03-23 19:42:04 +0000

    add configuration

---
+++ backend/CryptoDashboard/CryptoDashboard.Api/Configurations/HttpClientNames.cs
@@ -0,0 +1,8 @@
+namespace CryptoDashboard.Api.Configurations;
+
+public static class HttpClientNames
+{
+    public const string BinanceApi = "BinanceApi";
+    
+    public const string CoinGeckoApi = "CoinGeckoApi";
+}
--- backend/CryptoDashboard/CryptoDashboard.Api/Program.cs
+++ backend/CryptoDashboard/CryptoDashboard.Api/Program.cs
@@ -1,5 +1,6 @@
 using CryptoDashboard.Api.Models;
 using CryptoDashboard.Api.Services;
+using CryptoDashboard.Api.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.Configure<BinanceOptions>(builder.Configuration.GetSection(BinanceOptions.ConfigKey));
 builder.Services.AddHostedService<BinanceWorkerService>();
+builder.Services.Configure<CoinGeckoOptions>(builder.Configuration.GetSection(CoinGeckoOptions.ConfigKey));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
